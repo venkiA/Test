6500679e38d16d2087fcc70b4dbaa246ca5b634c
venkiA <34596176+venkiA@users.noreply.github.com>
2019-01-31 14:59:36 +0530

    Update TestGreeter.java

--- maven-project/server/src/test/java/com/example/TestGreeter.java
+++ maven-project/server/src/test/java/com/example/TestGreeter.java
@@ -23,7 +23,6 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
